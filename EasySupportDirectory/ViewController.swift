c0069f3aae877f359d8f8f8039fca76e099d4bae
Jon Worms <jon_worms@icloud.com>
2018-05-18 22:00:04 -0600

    Initial Commit

---
+++ EasySupportDirectory/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EasySupportDirectory
+//
+//  Created by Jon Worms on 5/18/18.
+//  Copyright © 2018 Jon Worms. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
